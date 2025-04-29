56840a20a11c52f152032cb8229e77b4cbf95eca
Roger <roverlb@live.com>
2025-04-29 09:53:48 -0500

    feat(20250419): fix queries get table 2

--- src/main/java/com/ayesa/batch/mappers/Table2Mapper.java
+++ src/main/java/com/ayesa/batch/mappers/Table2Mapper.java
@@ -32,7 +32,6 @@ public class Table2Mapper extends AbstractEntityMapper {
         getValueByType(entity, resultSet, DESC_ACCION);
         getValueByType(entity, resultSet, COD_ESTADO_ATENCION);
         getValueByType(entity, resultSet, FEC_RESPUESTA);
-        getValueByType(entity, resultSet, NUM_RESOLUCION);
         getValueByType(entity, resultSet, COD_ESTADO);
 
         changeFormatDate(entity, FEC_CREACION, DateUtil.FORMAT_DATETIME_1, DateUtil.FORMAT_DATETIME_2);
